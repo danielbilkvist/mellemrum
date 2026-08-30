015d3284d89854c1c2ab975ebe43116a68959ae4
danielbilkvist <danielbilkvist@gmail.com>
2026-08-30 22:18:11 +0200

    Created navbar.module.css - Simplified the navbar css classes - Avoids class conflicts with other navigations and links, and scopes the navbar css to only affect the navbar component - Simplifies main.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav className="site-nav">
-      <NavLink className="brand" to="/">
+    <nav className={styles.nav}>
+      <NavLink className={styles.brand} to="/">
         mellemrum<span>.</span>
       </NavLink>
-      <div className="nav-links">
+      <div className={styles.links}>
         <NavLink to="/">Events</NavLink>
         <NavLink to="/om">Om Mellemrum</NavLink>
       </div>
