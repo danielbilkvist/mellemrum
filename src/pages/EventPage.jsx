8514c41f012df90e8c1988ecc02a66fbc1788b74
danielbilkvist <danielbilkvist@gmail.com>
2026-09-01 10:20:52 +0200

    event/venue supabase split

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -18,9 +18,11 @@ export default function EventPage() {
 
   useEffect(() => {
     async function getEvent() {
-      const response = await fetch(`${SUPABASE_URL}/events?id=eq.${eventId}`, {
-        headers,
-      });
+      const query = "select=*,venue:venues(*)";
+        const response = await fetch(
+        `${SUPABASE_URL}/events?id=eq.${eventId}&${query}`,
+        { headers },
+        );
       const data = await response.json();
       setEvent(data[0]);
     }
@@ -89,10 +91,10 @@ export default function EventPage() {
               <p>
                 <strong>Sted</strong>
                 <span>
-                  {event.venueName}
+                  {event.venue.name}
                   <br />
-                  {event.venueAddress}, {event.venuePostalCode}{" "}
-                  {event.venueCity}
+                  {event.venue.address}, {event.venue.postalCode}{" "}
+                  {event.venue.city}
                   {event.venueWebsite && (
                     <>
                       <br />
