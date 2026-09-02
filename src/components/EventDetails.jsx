5408a447bdf7c2cf8ad5aa9547cfb34a34ec2a27
danielbilkvist <danielbilkvist@gmail.com>
2026-09-02 22:12:37 +0200

    Created EventDetails component to manage the eventDetail section

---
+++ src/components/EventDetails.jsx
@@ -0,0 +1,39 @@
+import { formatEventDate } from "../utils/formatDate";
+import { formatEventTime } from "../utils/formatDate";
+
+export default function EventDetails({ event }) {
+    return (
+<section className="event-detail">
+  <img src={event.image} alt="" />
+  <div className="event-detail-content">
+    <p className="event-category">{event.category}</p>
+    <h1>{event.title}</h1>
+    <p className="lead">{event.summary}</p>
+    <div className="detail-list">
+      <p>
+        <strong>Dato</strong>
+        {formatEventDate(event.date)} kl. {formatEventTime(event.date)}
+      </p>
+      <p>
+        <strong>Sted</strong>
+        <span>
+          {event.venue.name}
+          <br />
+          {event.venue.address}, {event.venue.postalCode} {event.venue.city}
+          {event.venue.website && (
+            <>
+              <br />
+              <a href={event.venue.website}>Besøg venue</a>
+            </>
+          )}
+        </span>
+      </p>
+      <p>
+        <strong>Pris</strong>
+        {event.price === 0 ? "Gratis" : `${event.price} kr.`}
+      </p>
+    </div>
+    <p>{event.description}</p>
+  </div>
+</section>);
+}
--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,8 +1,8 @@
 import { Link, useParams } from "react-router";
 import useEvent from "../hooks/useEvent";
+import EventDetails from "../components/EventDetails";
 import Registration from "../components/Registration";
-import { formatEventDate } from "../utils/formatDate";
-import { formatEventTime } from "../utils/formatDate";
+
 
 export default function EventPage() {
   const { eventId } = useParams();
@@ -28,42 +28,7 @@ export default function EventPage() {
           ← Alle events
         </Link>
 
-        <section className="event-detail">
-          <img src={event.image} alt="" />
-          <div className="event-detail-content">
-            <p className="event-category">{event.category}</p>
-            <h1>{event.title}</h1>
-            <p className="lead">{event.summary}</p>
-            <div className="detail-list">
-              <p>
-                <strong>Dato</strong>
-                {formatEventDate(event.date)}{" "}
-                kl.{" "}
-                {formatEventTime(event.date)}
-              </p>
-              <p>
-                <strong>Sted</strong>
-                <span>
-                  {event.venue.name}
-                  <br />
-                  {event.venue.address}, {event.venue.postalCode}{" "}
-                  {event.venue.city}
-                  {event.venue.website && (
-                    <>
-                      <br />
-                      <a href={event.venue.website}>Besøg venue</a>
-                    </>
-                  )}
-                </span>
-              </p>
-              <p>
-                <strong>Pris</strong>
-                {event.price === 0 ? "Gratis" : `${event.price} kr.`}
-              </p>
-            </div>
-            <p>{event.description}</p>
-          </div>
-        </section>
+        <EventDetails event={event} />
 
         <Registration event={event} />
 
