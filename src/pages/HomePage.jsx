c19704ef1242e56f48ddbab7686d5a05f038cd46
danielbilkvist <danielbilkvist@gmail.com>
2026-09-02 12:53:11 +0200

    Moved the useEvent function to a seperate js file to handle the useEvents function.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,46 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import useEvents from "../hooks/useEvents";
 import { Link } from "react-router";
 import EventGrid from "../components/EventGrid";
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const headers = {
-  apikey: import.meta.env.VITE_SUPABASE_APIKEY,
-  "Content-Type": "application/json"
-};
-
 export default function HomePage() {
-  const [events, setEvents] = useState([]);
-  const [search, setSearch] = useState("");
-  const [category, setCategory] = useState("Alle");
-
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    async function getEvents() {
-      try {
-        setLoading(true);
-        setError("");
-        
-      const query = "select=*,venue:venues(*)";
-
-      const response = await fetch(`${SUPABASE_URL}/events?order=date.asc&${query}`, { headers });
-      if (!response.ok) {
-        throw new Error("Siden kunne ikke hente events.");
-      }
-      const data = await response.json();
-      setEvents(data);
-      } catch (error) {
-        console.error(error);
-
-      setError("Der opstod en fejl under hentning af events. Prøv igen senere.");
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    getEvents();
-  }, []);
+  const { events, loading, error } = useEvents();
+   const [search, setSearch] = useState("");
+   const [category, setCategory] = useState("Alle");
 
   const categories = ["Alle", ...new Set(events.map((event) => event.category))];
 
