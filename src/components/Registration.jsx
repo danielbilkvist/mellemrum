5aa5449fc9a8f1cd7f5d9091e53a326f0c6188ac
danielbilkvist <danielbilkvist@gmail.com>
2026-09-03 14:41:36 +0200

    Created modules and moved css from main to registration module

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { create } from "../services/registrations";
+import style from "./Registration.module.css";
 
 export default function Registration({ event }) {
 const [name, setName] = useState("");
@@ -32,36 +33,38 @@ async function handleSubmit(eventSubmit) {
   }
 
   return (
-     <section className="signup-panel">
-          <div>
-            <p className="eyebrow dark">Tilmelding</p>
-            <h2>Reserver din plads</h2>
-            <p>
-              Udfyld formularen, så sender vi din tilmelding til arrangøren.
-            </p>
-          </div>
+    <section className={style.registration}>
+      <div>
+        <p className="eyebrow dark">Tilmelding</p>
+        <h2>Reserver din plads</h2>
+        <p>Udfyld formularen, så sender vi din tilmelding til arrangøren.</p>
+      </div>
 
-          <form onSubmit={handleSubmit}>
-            <label>
-              Navn
-              <input
-                required
-                value={name}
-                onChange={(inputEvent) => setName(inputEvent.target.value)}
-              />
-            </label>
-            <span>E-mail</span>
-            <input
-              required
-              type="email"
-              value={email}
-              onChange={(inputEvent) => setEmail(inputEvent.target.value)}
-              placeholder="dig@example.com"
-            />
-            <button type="submit" disabled={isSubmitting}>
-              {isSubmitting ? "Gemmer..." : "Tilmeld mig"}
-            </button>
-            {submitMessage && <span role="status">{submitMessage}</span>}
-          </form>
-        </section>);
+      <form onSubmit={handleSubmit}>
+        <span>Navn</span>
+        <label>
+          <input
+            className={style.input}
+            required
+            value={name}
+            onChange={(inputEvent) => setName(inputEvent.target.value)}
+            placeholder="Dit navn"
+          />
+        </label>
+        <span>E-mail</span>
+        <input
+          className={style.input}
+          required
+          type="email"
+          value={email}
+          onChange={(inputEvent) => setEmail(inputEvent.target.value)}
+          placeholder="dig@example.com"
+        />
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Gemmer..." : "Tilmeld mig"}
+        </button>
+        {submitMessage && <span role="status">{submitMessage}</span>}
+      </form>
+    </section>
+  );
         }
