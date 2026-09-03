61478909eb1ea7579b3cbadc8de75e26716fc8f0
danielbilkvist <danielbilkvist@gmail.com>
2026-09-03 15:07:46 +0200

    Moved css from main to EventCard

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router";
 import { formatEventDate } from "../utils/formatDate";
+import style from "./EventCard.module.css";
 
 export default function EventCard({ event }) {
     return (
-        <article className="event-card" key={event.id}>
+        <article className={style.card} key={event.id}>
               <img src={event.image} alt="" />
-              <div className="event-card-content">
+              <div className={style.cardContent}>
                 <p className="event-category">{event.category}</p>
                 <h3>{event.title}</h3>
                 <p>{event.summary}</p>
@@ -13,7 +14,7 @@ export default function EventCard({ event }) {
                   <span>{formatEventDate(event.date)}</span>
                   <span>{event.venue.name}</span>
                 </div>
-                <Link className="card-link" to={`/events/${event.id}`}>
+                <Link className={style.link} to={`/events/${event.id}`}>
                   Læs mere
                 </Link>
               </div>
