a7182dbb542f4c6f73e4677c94f8d63a3cf9fdbe
danielbilkvist <danielbilkvist@gmail.com>
2026-09-03 14:45:54 +0200

    Moved css from main to eventGrid. Moved @media css from main to registrations

--- src/components/EventGrid.jsx
+++ src/components/EventGrid.jsx
@@ -1,8 +1,9 @@
 import EventCard from './EventCard';
+import style from './EventGrid.module.css';
 
 export default function EventGrid({ events }) {
     return (
-         <section className="event-grid">
+         <section className={style.grid}>
           {events.map((event) => (
             <EventCard key={event.id} event={event} />
           ))}
