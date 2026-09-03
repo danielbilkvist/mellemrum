e922a725fdbb5733be4d8f5784dbeaa7c68aca8c
danielbilkvist <danielbilkvist@gmail.com>
2026-09-03 14:48:42 +0200

    moved css from main to EventFIlters

--- src/components/EventFilters.jsx
+++ src/components/EventFilters.jsx
@@ -1,26 +1,34 @@
-export default function EventFilters({ search, setSearch, category, setCategory, categories }) {
+import style from "./EventFilters.module.css";
+
+export default function EventFilters({
+  search,
+  setSearch,
+  category,
+  setCategory,
+  categories,
+}) {
   return (
-            <section className="filters">
-          <label>
-            Søg
-            <input
-              type="search"
-              value={search}
-              onChange={(event) => setSearch(event.target.value)}
-              placeholder="Søg efter titel eller sted"
-            />
-          </label>
-          <label>
-            Kategori
-            <select
-              value={category}
-              onChange={(event) => setCategory(event.target.value)}
-            >
-              {categories.map((item) => (
-                <option key={item}>{item}</option>
-              ))}
-            </select>
-          </label>
-        </section>
-    );
+    <section className={style.filter}>
+      <label>
+        Søg
+        <input
+          type="search"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+          placeholder="Søg efter titel eller sted"
+        />
+      </label>
+      <label>
+        Kategori
+        <select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          {categories.map((item) => (
+            <option key={item}>{item}</option>
+          ))}
+        </select>
+      </label>
+    </section>
+  );
 }
